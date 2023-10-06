d3147e9fded03ca66f1027141e60d62b1cd17303
Jayashanka Deshan <maxwon555@gmail.com>
2023-10-06 11:16:14 +0530

    commit files to the git

---
+++ src/main/java/com/mongo/api/v01/MongoApiApplication.java
@@ -0,0 +1,16 @@
+package com.mongo.api.v01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+@SpringBootApplication
+@EntityScan("com.mongo.api.v01.entity")
+@EnableMongoRepositories("com.mongo.api.v01.repositories")
+public class MongoApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MongoApiApplication.class, args);
+    }
+
+}
