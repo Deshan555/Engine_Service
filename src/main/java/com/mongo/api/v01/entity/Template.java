a6cb35da7b08cac12cd3a6d84c1458ce58f7576a
Jayashanka Deshan <maxwon555@gmail.com>
2023-10-06 10:16:16 +0530

    rename MailTemplate.java to Template.java

---
+++ src/main/java/com/mongo/api/v01/entity/Template.java
@@ -0,0 +1,24 @@
+package com.mongo.api.v01.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Document("mailTemplates")
+public class Template {
+    @Id
+    private Long id;
+    private String templateName;
+    private String subject;
+    private String body;
+    private String templateType;
+    private LocalDateTime createdDate;
+}
